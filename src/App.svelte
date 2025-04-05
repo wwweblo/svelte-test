0bef088217194cfa7a88ca0ae9a664d21cdc46bc
Yarik <coffe1n21@gmail.com>
2025-04-05 22:12:09 +0700

    basic

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,62 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	import { each } from "svelte/internal";
+  
+	  let showUsers = false;
+	  let users = [
+		  {id: 1, name: "Bob"},
+		  {id: 2, name: "Alice"},
+		  {id: 3, name: "Ben"},
+	  ];
+  
+	  function addUser (name) {
+		  if (name) {
+			  const newUser  = {
+				  id: users.length + 1,
+				  name: name
+			  };
+			  users = [...users, newUser ];
+		  }
+	  }
+  
+	  function toggleBool() {
+		  showUsers = !showUsers;
+	  }
+  </script>
+  
+  <main>
+	  <section>
+		  <input
+			  type="text"
+			  id="userInput"
+			  placeholder="Enter the name..." >
+		  <button on:click={() => addUser (document.getElementById('userInput').value)}>
+			  submit
+		  </button>
+	  </section>
+	  <section class="horisontalFlex">
+		  <input
+			  type="checkbox"
+			  name="showUsersBox" 
+			  id="showUsersBox"
+			  on:change={toggleBool}
+		  >
+		  <label for="showUsersBox">Show Names</label>
+	  </section>
+	  {#if showUsers}
+		  <ol>
+			  {#each users as user (user.id)}
+				  <li>
+					  {user.name}
+				  </li>
+			  {/each}
+		  </ol>
+	  {/if}
+  </main>
+  
+  <style>
+	  .horisontalFlex {
+		  display: flex;
+		  flex-direction: row;
+		  gap: 1rem;
+	  }
+  </style>
